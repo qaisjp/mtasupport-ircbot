b55d69cadab2331862502639201afacb83062dfa
Qais Patankar <qaisjp@gmail.com>
2014-05-14 14:14:06 +0100

    Update ircInit.js

--- ircInit.js
+++ ircInit.js
@@ -2,7 +2,7 @@ CHANNEL = "#mta.support";
 irc = require('irc');
 client = new irc.Client('irc.gtanet.com', 'MTASupport', {
 	showErrors:true,
-	userName:"MTASupportBot",
-	realName: "qaisjp",
+	userName:"MTASupport",
+	realName: "MTASupport IRC Bot",
     channels: [CHANNEL],
 });
